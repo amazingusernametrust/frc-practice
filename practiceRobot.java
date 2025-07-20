c0b5c9cec9bd335b9268b3f016096e861113d51f
mia <spaceY1029@gmail.com>
2025-07-20 17:55:19 +0300

    gaming

---
+++ practiceRobot.java
@@ -0,0 +1,8 @@
+public class practiceRobot {
+    public static void main(String[] args) {
+        double time = 4.0;
+        double distance = 24.0;
+        double speed = distance/time; 
+        System.out.println(("is high velocity" + (Math.abs(speed)>3))); 
+    }
+}///slay
